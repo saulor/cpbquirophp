32a7431bb6838b42db0400d4e0f03c117004aa0d
Saulo R <sauloroliveira@gmail.com>
2015-12-29 16:44:15 -0300

    Alterei o nome do projeto.

--- config.php
+++ config.php
@@ -1,13 +1,13 @@
 <?php
 	session_start();
-	ini_set('display_errors', 0); 
-	error_reporting(E_ALL); 
+	ini_set('display_errors', 0);
+	error_reporting(E_ALL);
 	date_default_timezone_set('America/Sao_Paulo');
 	if (!isset($_SESSION["mensagemSe"])) {
 		$_SESSION["mensagemSe"] = array();
 	}
 	DEFINE("PREFIX", "cpq_");
-	DEFINE("WWW_ROOT", "http://localhost/~saulor/cpbquirophp");
+	DEFINE("WWW_ROOT", "http://localhost/~saulor/cpbquiro");
 	//DEFINE("WWW_ROOT", "http://sistema.centropbquiropraxia.com.br");
 	DEFINE("DIR_ROOT", dirname(__file__));
 	DEFINE("QUANTIDADE_POR_PAGINA", 10);
